7fab45eb77d5a9939b4147842a89e55f931be3cd
Serzhan Akhmetov <serzhan.akhmetov@gmail.com>
2020-01-14 21:54:12 +0600

    Added home page stubs: - create ABOUT page component (stub) - create CONTACT page component (stub) - create PRICING page component (stub)

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { HowitworksComponent } from './howitworks/howitworks.component';
 import { HomeComponent } from './home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 const routes: Routes = [
   { path: '',
@@ -11,6 +14,9 @@ const routes: Routes = [
       { path: '', redirectTo: 'start', pathMatch: 'full' },
       { path: 'start', component: StartComponent },
       { path: 'howitworks', component: HowitworksComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'pricing', component: PricingComponent },
     ]
   }
 ];
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,10 +8,25 @@ import { AuthModalComponent } from '../auth/auth-modal/auth-modal.component';
 import { StartComponent } from './start/start.component';
 import { HowitworksComponent } from './howitworks/howitworks.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
-  imports: [HomeRoutingModule, NgZorroAntdModule, CommonModule],
-  declarations: [HomeComponent, HeaderComponent, AuthModalComponent, StartComponent, HowitworksComponent],
-  exports: [HomeComponent, AuthModalComponent, HeaderComponent, StartComponent, HowitworksComponent],
+  imports: [
+    HomeRoutingModule,
+    NgZorroAntdModule,
+    CommonModule
+  ],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    AuthModalComponent,
+    StartComponent,
+    HowitworksComponent,
+    AboutComponent,
+    ContactComponent,
+    PricingComponent
+  ],
 })
 export class HomeModule { }
