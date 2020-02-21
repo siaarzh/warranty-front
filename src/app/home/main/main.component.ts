cba503fff7caf51b5b5b6716d1242426cc272ebc
Serzhan Akhmetov <serzhan.akhmetov@gmail.com>
2020-02-21 14:45:20 +0600

    - added login action to "Start Now" button

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthModalService } from '../../auth/auth-modal/auth-modal.service';
 
 @Component({
   selector: 'app-start',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.sass'],
 })
-export class MainComponent {}
+export class MainComponent {
+  constructor(private authModalSub: AuthModalService) {}
+
+  onLogin(): void {
+    this.authModalSub.show();
+  }
+}
