c73bbce705684721fa3bc921f4a3016c5930607f
Serzhan Akhmetov <serzhan.akhmetov@gmail.com>
2020-03-06 08:43:36 +0600

    home/contact: - created stub

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-about',
   templateUrl: './contact.component.html',
   styleUrls: ['../../shared/articles.sass', './contact.component.sass'],
 })
-export class ContactComponent {}
+export class ContactComponent {
+  fbPage = environment.company.social.fbPage;
+  vkPage = environment.company.social.vkPage;
+  igPage = environment.company.social.igPage;
+}
